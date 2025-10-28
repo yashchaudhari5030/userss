21e8acbd63cbb6926390519c54679d3f748f531f
yashchaudhari5030 <yashchaudhari5030@gmail.com>
2025-10-28 18:16:06 +0530

    Initial Commit

---
+++ Userss/src/main/java/com/users/demo/UserssApplication.java
@@ -0,0 +1,15 @@
+package com.users.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class UserssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserssApplication.class, args);
+	}
+
+}
---
+++ Userss/src/test/java/com/users/demo/UserssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.users.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
